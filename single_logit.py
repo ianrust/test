c6e254a3825ab4863ff3d0a9c9ea4daa31de91d9
Ian Rust <iancrust@gmail.com>
2020-08-14 11:04:51 -0700

    pkl

--- single_logit.py
+++ single_logit.py
@@ -9,7 +9,8 @@ model = tf.keras.Sequential(
         tf.keras.layers.Conv2D(32, (3, 3), padding="same", activation="relu"),
         tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
         tf.keras.layers.Conv2D(64, (3, 3), padding="same", activation="relu"),
-        tf.keras.layers.Dense(8, input_dim=4, activation='relu'),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(8, activation='relu'),
 	    tf.keras.layers.Dense(2, activation='softmax')
     ]
 )

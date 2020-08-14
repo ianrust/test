97249f9ea1c3bc83e23f12dfe526e53a4669fd97
Ian Rust <iancrust@gmail.com>
2020-08-14 10:18:36 -0700

    saving model

---
+++ single_logit.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+
+my_devices = tf.config.experimental.list_physical_devices(device_type='CPU')
+tf.config.experimental.set_visible_devices(devices= my_devices, device_type='CPU')
+
+model = tf.keras.Sequential(
+    [
+        tf.keras.layers.Conv2D(32, (3, 3), padding="same", activation="relu"),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Conv2D(64, (3, 3), padding="same", activation="relu"),
+        tf.keras.layers.Dense(8, input_dim=4, activation='relu'),
+	    tf.keras.layers.Dense(2, activation='softmax')
+    ]
+)
+
+model.build((1, None, None, 3))
+print(model.summary())
+model.save('./model.h5')
